89b471dcbcba19f4685673bdc82a06248b5711fd
Yauheni <ev.drozd.2020@mail.ru>
2021-07-22 20:29:41 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className={'header'}>
+            <img src='https://static.tildacdn.com/tild6139-3161-4530-a231-326236633830/unnamed.jpg'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Profile() {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src="https://kubnews.ru/upload/iblock/a61/a616a0977e9f98fff2f831fe7f316e24.jpg" alt=""/>
+            </div>
+            <div>
+                ava+discr
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
