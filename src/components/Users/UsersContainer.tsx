e1e20ecbf19319bd0c89721c20cb712ab07bc11b
Yauheni <ev.drozd.2020@mail.ru>
2022-01-11 20:09:14 +0300

    lesson 81

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -73,7 +73,6 @@ const mapStateToProps = (state: AppStateType) => {
     }
 }
 
-
 export default compose<ComponentType>(
     connect(mapStateToProps, {
         followTC, unfollowTC, changeCurrentPage,
